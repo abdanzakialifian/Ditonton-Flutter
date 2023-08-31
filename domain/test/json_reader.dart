da55b938991bf685760fd8e3b25663b4f403aaa1
abdanzakialifian <abdan.alifian@mncbank.co.id>
2023-08-31 14:54:46 +0700

    Add dependencies path_provider to module domain and presentation

--- domain/test/json_reader.dart
+++ domain/test/json_reader.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 String readJson(String name) {
-  var dir = "../domain";
+  var dir = Directory.current.path;
   if (dir.endsWith('/test')) {
     dir = dir.replaceAll('/test', '');
   }
