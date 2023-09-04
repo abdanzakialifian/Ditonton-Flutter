75247c31926413b0db08714c531bd006b20c9d94
abdanzakialifian <abdan.alifian@mncbank.co.id>
2023-09-04 10:14:06 +0700

    Add unit test in module data

---
+++ data/test/data/models/tvshowdetail/tv_show_detail_response_test.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:data/models/tv_show_detail/tv_show_detail_response.dart';
+import 'package:flutter_test/flutter_test.dart';
+import '../../../dummy_data/dummy_objects.dart';
+
+void main() {
+  test("should return tv show detail response json", () {
+    expect(jsonEncode(jsonDecode(dummyTvShowDetailJsonResponse)),
+        jsonEncode(dummyTvShowDetailResponse.toJson()));
+  });
+
+  test("should return tv shpw detail response", () {
+    expect(
+        TvShowDetailResponse.fromJson(
+            jsonDecode(dummyTvShowDetailJsonResponse)),
+        dummyTvShowDetailResponse);
+  });
+}
